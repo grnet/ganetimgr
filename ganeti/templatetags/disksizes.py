4e3ddac5f7b244a877c149f0078b2b7705e7e98d
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2011-06-02 14:22:32 +0300

    Add memsize tag and cleanup instance details
    Add memsize tag to use filesizeformat with numbers expressed in MB.
    Also further cleanup the instance detail template.
    
    Signed-off-by: Apollon Oikonomopoulos <apollon@noc.grnet.gr>

--- ganeti/templatetags/disksizes.py
+++ ganeti/templatetags/disksizes.py
@@ -4,6 +4,9 @@ from django.template.defaultfilters import filesizeformat
 register = template.Library()
 
 @register.filter
-# truncate after a certain number of characters
 def disksizes(value):
     return [filesizeformat(v * 1024**2) for v in value]
+
+@register.filter
+def memsize(value):
+    return filesizeformat(value * 1024**2)
