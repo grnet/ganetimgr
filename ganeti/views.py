grnet/ganetimgr

commit fa52ba1390d9eba0f0668e0bf6e04d9cb003541e

Ditch several import statements in views & models
Several import statements (some of them bringing a significant
dependency chain with them) were not used anymore but were mere remnants
of the past. Kill them for efficiency and neatness.

Signed-off-by: Faidon Liambotis <faidon@noc.grnet.gr>

ganeti/views.py
@@ -1,22 +1,21 @@
 import urllib2
-import os
 import re
 import socket
-from models import *
 from django import forms
 from django.core.cache import cache
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
-from ganetimgr.util.portforwarder import forward_port
 from django.core.context_processors import request
 from django.template.context import RequestContext
 from django.template.loader import get_template
+from django.utils import simplejson
+
+from ganetimgr.ganeti.models import *
+from ganetimgr.util.portforwarder import forward_port
 
 from gevent.pool import Pool
 from gevent.timeout import Timeout
-
-from django.utils import simplejson
 
 
 def cluster_overview(request):

ganeti/models.py
@@ -1,25 +1,12 @@
-import os
-import sys
-import urllib
-import urllib2
-
 from django.db import models
 from django.core.cache import cache
 from django.contrib.auth.models import User, Group
-from simplejson import JSONEncoder, JSONDecoder
-from time import sleep
-from ganetimgr.util.portforwarder import forward_port
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datetime import datetime
 import vapclient
 from socket import gethostbyname
-
 from util.ganeti_client import GanetiRapiClient, GenericCurlConfig
 from ganetimgr.settings import RAPI_CONNECT_TIMEOUT, RAPI_RESPONSE_TIMEOUT, GANETI_TAG_PREFIX
-
-
-dec = JSONDecoder()
-enc = JSONEncoder()
 
 
 class InstanceManager(object):

commit 93aeddff1e24cb2db1d26982e9fc5c7f98cc7ba1

Fix missing import for sleep()
sleep() was called in views.py but it was never imported. It used to
work but was broken with fa52ba1390d9eba0f0668e0bf6e04d9cb003541e (by
yours truly) which (rightly) removed the import statement from models
but which was indirectly imported into views since it imports * from
models.

ganeti/views.py
@@ -1,6 +1,7 @@
 import urllib2
 import re
 import socket
+from time import sleep
 from django import forms
 from django.core.cache import cache
 from django.contrib.auth import authenticate, login, logout