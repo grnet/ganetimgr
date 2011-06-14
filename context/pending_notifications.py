a5eb2239d6f708ec2b06d11e22d920aba8fb967f
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2011-06-14 19:47:41 +0300

    Export the pending application count via RequestContext
    Make the pending application count available through the
    pending_notifications.notify template context processor and use it to indicate
    the pending application count on the user action bar.
    
    Signed-off-by: Apollon Oikonomopoulos <apollon@noc.grnet.gr>

--- context/pending_notifications.py
+++ context/pending_notifications.py
@@ -6,12 +6,14 @@ from django.utils.safestring import mark_safe
 from ganetimgr.apply.models import InstanceApplication, STATUS_PENDING
 
 def notify(request):
+    res = {}
     if (request.user and
         request.user.has_perm("apply.change_instance_application")):
         pend = InstanceApplication.objects.filter(status=STATUS_PENDING)
         if pend:
+            count = pend.count()
             msg = "There are <a href=\"%s\">%d pending" \
-                  " applications</a>." % (reverse("application-list"),
-                                          pend.count())
+                  " applications</a>." % (reverse("application-list"), count)
             messages.add_message(request, messages.INFO, mark_safe(msg))
-    return {}
+            res.update(pending_count=count)
+    return res
