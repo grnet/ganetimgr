grnet/ganetimgr

commit 4c867cfb6445a8ddcbd22105e784403b5e0ca646

Make application comments compulsory
Move comments to a separate fieldset and make them compulsory. Update the help
text and relevant translations.

Signed-off-by: Apollon Oikonomopoulos <apollon@noc.grnet.gr>

apply/forms.py
@@ -56,7 +56,7 @@ class InstanceForm(forms.ModelForm):
 
 
 class InstanceApplicationForm(InstanceForm):
-    comments = forms.CharField(widget=forms.Textarea, required=False,
+    comments = forms.CharField(widget=forms.Textarea, required=True,
                                help_text=ugettext_lazy("Additional comments you would like"
                                          " the service administrators to see"), label=ugettext_lazy("Comments"))
     accept_tos = forms.BooleanField()
@@ -76,8 +76,7 @@ class InstanceApplicationForm(InstanceForm):
         if not (organization or
                 ("admin_contact_name" in self.cleaned_data and
                  "admin_contact_email" in self.cleaned_data and
-                 "admin_contact_phone" in self.cleaned_data and
-                 self.cleaned_data["comments"])):
+                 "admin_contact_phone" in self.cleaned_data)):
             raise forms.ValidationError(_("Choose either an organization or"
                                           " fill in the contact information"))
         return self.cleaned_data

locale/el/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2011-07-06 16:50+0300\n"
+"POT-Creation-Date: 2011-07-06 18:09+0300\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: Leonidas Poulopoulos <leopoul@noc.grnet.gr>\n"
 "Language-Team: GRNET NOC <noc@noc.grnet.gr>\n"
@@ -41,7 +41,7 @@ msgstr "Όνομα"
 msgid "Memory"
 msgstr "Μνήμη"
 
-#: apply/forms.py:29 apply/forms.py:90
+#: apply/forms.py:29 apply/forms.py:89
 msgid "Disk size (GB)"
 msgstr "Μέγεθος δίσκου (GB)"
 
@@ -78,61 +78,56 @@ msgstr ""
 msgid "Comments"
 msgstr "Σχόλια"
 
-#: apply/forms.py:81
+#: apply/forms.py:80
 msgid "Choose either an organization or fill in the contact information"
 msgstr ""
 "Παρακαλούμε επιλέξτε ένα φορέα ή συμπληρώστε χειροκίνητα τα στοιχεία "
 "επικοινωνίας του διαχειριστικού υπεύθυνου"
 
-#: apply/forms.py:98
+#: apply/forms.py:97
 msgid "Please specify a network"
 msgstr "Επιλέξτε ένα δίκτυο για τη σύνδεση της εικονικής μηχανής"
 
-#: apply/forms.py:104
+#: apply/forms.py:103
 msgid "Please specify a reason for rejection"
 msgstr "Παρακαλώ καθορίστε το λόγο της απόρριψης της αίτησης"
 
-#: apply/forms.py:110
+#: apply/forms.py:109
 msgid "Application already handled"
 msgstr ""
 
-#: apply/forms.py:130
+#: apply/forms.py:129
 msgid "Malformed SSH key, must be in OpenSSH format, RSA or DSA"
 msgstr "Εσφαλμένο SSK κλειδί. Θα πρέπει να είναι σε μορφή OpenSSH, RSA ή DSA"
 
-#: apply/forms.py:143
+#: apply/forms.py:142
 msgid "Malformed SSH key"
 msgstr "Εσφαλμένη μορφή SSH κλειδιού"
 
-#: apply/forms.py:149
+#: apply/forms.py:148
 msgid "Invalid RSA SSH key"
 msgstr "Λανθασμένο RSA SSH key"
 
-#: apply/forms.py:154
+#: apply/forms.py:153
 msgid "Invalid DSS SSH key"
 msgstr "Λανθασμένο DSS SSH key"
 
-#: apply/forms.py:156
+#: apply/forms.py:155
 #, python-format
 msgid "Unknown key type '%s'"
 msgstr "Άγνωστος τύπος κλειδιού '%s'"
 
-#: apply/forms.py:164 templates/review.html:50 templates/review.html.py:108
+#: apply/forms.py:163 templates/review.html:50 templates/review.html.py:108
 msgid "Email"
 msgstr "Email"
 
-#: apply/forms.py:165
+#: apply/forms.py:164
 msgid "Email (verify)"
 msgstr "Email (επιβεβαίωση)"
 
-#: apply/forms.py:173
+#: apply/forms.py:172
 msgid "Mail fields do not match."
 msgstr "Τα πεδία e-mail δεν ταιριάζουν"
-
-#: apply/forms.py:166 templates/apply.html:60 templates/review.html:49
-#: templates/review.html.py:107 templates/user_instances.html:23
-msgid "Name"
-msgstr "Όνομα"
 
 #: apply/forms.py:167
 msgid "Surname"
@@ -363,12 +358,28 @@ msgstr "Αίτηση για νέα εικονική μηχανή"
 msgid "Instance information"
 msgstr "Στοιχεία εικονικής μηχανής"
 
-#: templates/apply.html:43 templates/apply.html.py:59 templates/review.html:47
+#: templates/apply.html:43
+msgid "Use/Comments"
+msgstr "Χρήση/Σχόλια"
+
+#: templates/apply.html:44
+msgid ""
+"\n"
+"<p>Give a short description of the intended use of this virtual machine, "
+"that justifies the parameter selection above. Feel free to include any "
+"additional comments.</p>\n"
+msgstr ""
+"\n"
+"<p>Δώστε μια σύντομη περιγραφή της χρήσης της εικονικής μηχανής, η οποία "
+"να δικαιολογεί την επιλογή παραμέτρων που κάνατε. Μπορείτε επίσης να "
+"περιλάβετε τυχόν σχόλια προς τους διαχειριστές της εφαρμογής.</p>\n"
+
+#: templates/apply.html:53 templates/apply.html.py:69 templates/review.html:47
 #: templates/review.html.py:105
 msgid "Administrative contact"
 msgstr "Διαχειριστικός υπεύθυνος"
 
-#: templates/apply.html:44
+#: templates/apply.html:54
 msgid ""
 "\n"
 "<p>If you are applying on behalf of a NOC under GRNET's constituency, please "
@@ -379,23 +390,27 @@ msgstr ""
 "<p>Αν κάνετε αίτηση για λογαριασμό του NOC κάποιου από τους Φορείς του ΕΔΕΤ, "
 "τότε παρακαλούμε να επιλέξετε τον αντίστοιχο φορέα. Σε διαφορετική "
 "περίπτωση, παρακαλούμε συμπληρώστε τα στοιχεία επικοινωνίας του "
-"διαχειριστικού υπεύθυνου και δηλώστε τη χρήση της εικονικής μηχανής και το "
-"Φορέα για τον οποίο προορίζεται στο πεδίο «Σχόλια».</p>\n"
+"διαχειριστικού υπεύθυνου.</p>\n"
 
-#: templates/apply.html:56
+#: templates/apply.html:66
 msgid "OR"
 msgstr "Ή"
 
-#: templates/apply.html:62 templates/review.html:51
+#: templates/apply.html:70 templates/review.html:49
+#: templates/review.html.py:107 templates/user_instances.html:23
+msgid "Name"
+msgstr "Όνομα"
+
+#: templates/apply.html:72 templates/review.html:51
 #: templates/review.html.py:109
 msgid "Phone"
 msgstr "Τηλέφωνο"
 
-#: templates/apply.html:68
+#: templates/apply.html:77
 msgid "Miscellaneous"
 msgstr "Λοιπά"
 
-#: templates/apply.html:69
+#: templates/apply.html:78
 msgid ""
 "\n"
 "<p>We kindly remind you of the following:</p>\n"
@@ -417,19 +432,19 @@ msgstr ""
 "υποδομή μας.</li>\n"
 "</ul>\n"
 
-#: templates/apply.html:76
+#: templates/apply.html:85
 msgid "I have read the above and accept them, along with the"
 msgstr "Διάβασα τα παραπάνω και τα αποδέχομαι μαζί με τους"
 
-#: templates/apply.html:76
+#: templates/apply.html:85
 msgid "Terms of Service"
 msgstr "Όρους Χρήσης της Υπηρεσίας"
 
-#: templates/apply.html:79
+#: templates/apply.html:88
 msgid "You must accept the terms of service before continuing."
 msgstr "Πρέπει να αποδεχθείτε τους Όρους Χρήσης πρoτού συνεχίσετε."
 
-#: templates/apply.html:84
+#: templates/apply.html:93
 msgid "Apply"
 msgstr "Υποβολή"
 
@@ -682,9 +697,8 @@ msgid "All"
 msgstr "Όλων"
 
 #: templates/user_instances.html:24
-#, fuzzy
 msgid "IP address"
-msgstr "Διεύθυνση email"
+msgstr "Διεύθυνση IP"
 
 #: templates/user_keys.html:86
 msgid ""
@@ -727,9 +741,8 @@ msgid "Activation Complete"
 msgstr "Η ενεργοποίηση ολοκληρώθηκε"
 
 #: templates/registration/activate.html:7
-#, fuzzy
 msgid "Activation Status"
-msgstr "Η ενεργοποίηση απέτυχε"
+msgstr "Ενεργοποίηση χρήστη"
 
 #: templates/registration/activate.html:8
 msgid "The user has probably been already activated."

static/css/base.css
@@ -237,6 +237,10 @@ form.update textarea {
 	width: 100%;
 }
 
+textarea#id_comments {
+	width: 80%;
+}
+
 .hidden {
 	display: none;
 }

templates/apply.html
@@ -40,6 +40,16 @@ th {
 </fieldset>
 
 <fieldset>
+<legend>{% trans "Use/Comments" %}</legend>
+{% blocktrans %}
+<p>Give a short description of the intended use of this virtual machine, that justifies the parameter selection above. Feel free to include any additional comments.</p>
+{% endblocktrans %}
+<p>{{ form.comments }}
+{% if form.errors %}<br /><span class="error">{{ form.comments.errors|join:", " }}</span>{% endif %}
+</p>
+</fieldset>
+
+<fieldset>
 <legend>{% trans "Administrative contact" %}</legend>
 {% blocktrans %}
 <p>If you are applying on behalf of a NOC under GRNET's constituency, please select the appropriate organization. Otherwise, fill-in the admin contact information below.</p>
@@ -60,7 +70,6 @@ th {
 <tr><th>{% trans "Name" %}</th><td>{{ form.admin_contact_name }}<span class="error">{{ form.admin_contact_name.errors|join:", " }}</span></td></tr>
 <tr><th>E-mail</th><td>{{ form.admin_contact_email }}<span class="error">{{ form.admin_contact_email.errors|join:", " }}</span></td></tr>
 <tr><th>{% trans "Phone" %}</th><td>{{ form.admin_contact_phone }}<span class="error">{{ form.admin_contact_phone.errors|join:", " }}</span></td></tr>
-<tr><th>{{ form.comments.label_tag }}</th><td>{{ form.comments }}<span class="error">{{ form.comments.errors|join:", " }}</span></td></tr>
 </table>
 </fieldset>
 

commit 7cc8b69689b1fa9c6e96034df37220ac652caf4f

Fix application form validation
4c867cf broke the application form validation, allowing an application with
neither an organization, nor an administrative contact.

We fix this behaviour.

Signed-off-by: Apollon Oikonomopoulos <apollon@noc.grnet.gr>

apply/forms.py
@@ -74,9 +74,9 @@ class InstanceApplicationForm(InstanceForm):
         organization = self.cleaned_data.get("organization", None)
 
         if not (organization or
-                ("admin_contact_name" in self.cleaned_data and
-                 "admin_contact_email" in self.cleaned_data and
-                 "admin_contact_phone" in self.cleaned_data)):
+                (self.cleaned_data.get("admin_contact_name", None) and
+                 self.cleaned_data.get("admin_contact_email", None) and
+                 self.cleaned_data.get("admin_contact_phone", None))):
             raise forms.ValidationError(_("Choose either an organization or"
                                           " fill in the contact information"))
         return self.cleaned_data