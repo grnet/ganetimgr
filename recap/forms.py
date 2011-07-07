626f901e606900856859887ab4203a8b2e2c59d3
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2011-07-07 13:10:48 +0300

    Use external recaptcha library
    Remove the recap application and depend on the external python-recaptcha
    library.
    
    Move the registration form to accounts/forms and update urls.py.dist
    accordingly.
    
    Together with 54f4a6b, this closes #757.
    
    Signed-off-by: Apollon Oikonomopoulos <apollon@noc.grnet.gr>

--- recap/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from recap import field as recap_field
-from registration.forms import RegistrationForm
-from django.utils.translation import ugettext_lazy as _
-
-class RecaptchaRegistrationForm(RegistrationForm):
-    recaptcha = recap_field.ReCaptchaField(label=_("Verify"))
