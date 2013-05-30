0d02bdf3fb67486fcae02218fb502d95e40f003f
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-05-30 15:09:54 +0300

    Add a node role parser in template tag

---
+++ ganeti/templatetags/noderole.py
@@ -0,0 +1,45 @@
+#
+# -*- coding: utf-8 -*- vim:fileencoding=utf-8:
+# Copyright © 2010-2012 Greek Research and Technology Network (GRNET S.A.)
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH REGARD
+# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+# USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THIS SOFTWARE.
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def noderole(value):
+    if value == "M":
+        return "Master"
+    if value == "C":
+        return "Master Candidate"
+    if value == "R":
+        return "Regular"
+    if value == "D":
+        return "Drained"
+    if value == "O":
+        return "Offline"
+
+@register.filter
+def nodelabel(value):
+    if value == "M":
+        return "success"
+    if value == "C":
+        return "info"
+    if value == "R":
+        return ""
+    if value == "D":
+        return "warning"
+    if value == "O":
+        return "important"
