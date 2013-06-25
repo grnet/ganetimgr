5dc613df116487fd05705e435f98247eb95a0afc
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-06-25 11:15:16 +0300

    Add user check in instance oweners view

--- stats/views.py
+++ stats/views.py
@@ -17,18 +17,21 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 
 from ganetimgr.ganeti.models import Cluster
 
 
-@user_passes_test(lambda u: u.is_staff)
 def instance_owners(request):
-    instances = [i for i in Cluster.get_all_instances() if i.users]
-    def cmp_users(x, y):
-        return cmp(",".join([ u.username for u in x.users]),
-                   ",".join([ u.username for u in y.users]))
-    instances.sort(cmp=cmp_users)
-
-    return render_to_response("instance_owners.html",
-                              {"instances": instances},
-                              context_instance=RequestContext(request))
+    if request.user.is_superuser or request.user.has_perm('ganeti.view_instances'):
+        instances = [i for i in Cluster.get_all_instances() if i.users]
+        def cmp_users(x, y):
+            return cmp(",".join([ u.username for u in x.users]),
+                       ",".join([ u.username for u in y.users]))
+        instances.sort(cmp=cmp_users)
+    
+        return render_to_response("instance_owners.html",
+                                  {"instances": instances},
+                                  context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect(reverse('user-instances'))
