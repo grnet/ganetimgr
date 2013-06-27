65852c028a466b67eb8a8e61d9e7bcef18535732
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-06-27 10:10:34 +0300

    Major statistics enhancements

--- ganeti/views.py
+++ ganeti/views.py
@@ -203,7 +203,7 @@ def user_sum_stats(request):
     j = Pool(80)
     user = request.user
     def _get_instance_details(instance):
-        t = Timeout(2)
+        t = Timeout(5)
         t.start()
         try:
             instancedetails.extend(generate_json_light(instance, user))
@@ -261,8 +261,10 @@ def generate_json(instance, user):
                         ))
 
     inst_dict['name'] =  i.name
-    inst_dict['cluster'] = i.cluster.description
-    inst_dict['clusterslug'] = i.cluster.slug
+    if user.is_superuser or user.has_perm('ganeti.view_instances'):
+        inst_dict['cluster'] = i.cluster.slug
+    else:
+        inst_dict['cluster'] = i.cluster.description
     inst_dict['memory'] = memsize(i.beparams['memory'])
     inst_dict['vcpus'] = i.beparams['vcpus']
     inst_dict['ipaddress'] = i.nic_ips[0] if i.nic_ips[0] else "-"
@@ -538,6 +540,9 @@ def instance(request, cluster_slug, instance):
 #                               'configform': configform,
 #                               'user': request.user})
 #    else:
+    instance.cpu_url = reverse(graph, args=(cluster.slug, instance.name,'cpu-ts'))
+    instance.net_url = reverse(graph, args=(cluster.slug, instance.name,'net-ts'))
+
     return render_to_response("instance.html",
                               {'cluster': cluster,
                                'instance': instance,
@@ -688,8 +693,8 @@ def prepare_tags(taglist):
 
 @login_required
 def stats(request):
+    clusters = Cluster.objects.all()
     if (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
-        clusters = Cluster.objects.all()
         instances = cache.get('leninstances')
         if instances is None:
             instances = len(Instance.objects.all())
@@ -719,80 +724,82 @@ def stats(request):
                                                       'orgs': orgs},
                                   context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect(reverse('user-instances'))
+        return render_to_response('statistics.html',
+                                  {'clusters':clusters},  
+                                  context_instance=RequestContext(request))
 
 @login_required
 def stats_ajax_instances(request):
-    if (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
-        cluster_list = cache.get('ajaxinstances')
-        if cluster_list is None:
-            clusters = Cluster.objects.all()
-            inst_dates = []
-            ret = []
+    username = request.user.username
+    cluster_list = cache.get('%s:ajaxinstances' %username)
+    if cluster_list is None:
+        clusters = Cluster.objects.all()
+        inst_dates = []
+        ret = []
+        i = 0
+        cluster_list = []
+        for cluster in clusters:
             i = 0
-            cluster_list = []
-            for cluster in clusters:
-                i = 0
-                cluster_dict = {}
+            cluster_dict = {}
+            if (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
                 cluster_dict['name'] = cluster.slug
-                cluster_dict['instances'] = []
-                for instance in cluster.get_user_instances(request.user):
-                    inst_dict = {}
-                    inst_dict['time'] = (1000*mktime(instance.ctime.timetuple()))
-                    inst_dict['name'] = instance.name
-                    cluster_dict['instances'].append(inst_dict)
-                cluster_dict['instances'] = sorted(cluster_dict['instances'], key=itemgetter('time'))
-                for item in cluster_dict['instances']:
-                    i = i + 1
-                    item['count'] = i
-                cluster_list.append(cluster_dict)
-            cache.set('ajaxinstances', cluster_list, 90)
-        return HttpResponse(json.dumps(cluster_list), mimetype='application/json')
-    else:
-        return HttpResponse(json.dumps([]), mimetype='application/json')
+            else:
+                cluster_dict['name'] = cluster.description
+            cluster_dict['instances'] = []
+            for instance in cluster.get_user_instances(request.user):
+                inst_dict = {}
+                inst_dict['time'] = (1000*mktime(instance.ctime.timetuple()))
+                inst_dict['name'] = instance.name
+                cluster_dict['instances'].append(inst_dict)
+            cluster_dict['instances'] = sorted(cluster_dict['instances'], key=itemgetter('time'))
+            for item in cluster_dict['instances']:
+                i = i + 1
+                item['count'] = i
+            cluster_list.append(cluster_dict)
+        cache.set('%s:ajaxinstances' %username, cluster_list, 90)
+    return HttpResponse(json.dumps(cluster_list), mimetype='application/json')
 
 @login_required
 def stats_ajax_vms_per_cluster(request, cluster_slug):
-    if (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
-        cluster_dict = cache.get('ajaxvmscluster:%s'%cluster_slug)
-        if cluster_dict is None:
-            cluster = Cluster.objects.get(slug=cluster_slug)
-            inst_dates = []
-            ret = []
-            i = 0
-            cluster_dict = {}
-            cluster_dict['name'] = cluster.slug
-            cluster_dict['instances'] = {'up':0,'down':0}
-            for instance in cluster.get_user_instances(request.user):
-                if instance.admin_state:
-                    cluster_dict['instances']['up'] = cluster_dict['instances']['up'] + 1
-                else:
-                    cluster_dict['instances']['down'] = cluster_dict['instances']['down'] + 1
-            cache.set('ajaxvmscluster:%s'%cluster_slug, cluster_dict, 90)
-        return HttpResponse(json.dumps(cluster_dict), mimetype='application/json')
-    else:
-        return HttpResponse(json.dumps([]), mimetype='application/json')
+    cluster_dict = cache.get('%s:ajaxvmscluster:%s'%(request.user.username, cluster_slug))
+    if cluster_dict is None:
+        cluster = Cluster.objects.get(slug=cluster_slug)
+        inst_dates = []
+        ret = []
+        i = 0
+        cluster_dict = {}
+        cluster_dict['name'] = cluster.slug
+        cluster_dict['instances'] = {'up':0,'down':0}
+        for instance in cluster.get_user_instances(request.user):
+            if instance.admin_state:
+                cluster_dict['instances']['up'] = cluster_dict['instances']['up'] + 1
+            else:
+                cluster_dict['instances']['down'] = cluster_dict['instances']['down'] + 1
+        cache.set('%s:ajaxvmscluster:%s'%(request.user.username, cluster_slug), cluster_dict, 90)
+    return HttpResponse(json.dumps(cluster_dict), mimetype='application/json')
+
 
 @login_required
 def stats_ajax_applications(request):
-    if (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
-        app_list = cache.get('ajaxapplist')
-        if app_list is None:
-            applications = InstanceApplication.objects.all().order_by('filed')
-            app_list = []
-            i = 0
-            for app in applications:
-               appd = {}
-               appd['instance'] = app.hostname
-               i = i + 1
-               appd['count'] = i
-               appd['user'] = app.applicant.username
-               appd['time'] = (1000*mktime(app.filed.timetuple()))
-               app_list.append(appd)
-            cache.set('ajaxapplist',app_list, 90)
-        return HttpResponse(json.dumps(app_list), mimetype='application/json')
-    else:
-        return HttpResponse(json.dumps([]), mimetype='application/json')
+    username = request.user.username
+    app_list = cache.get('%s:ajaxapplist' %username)
+    if app_list is None:
+        applications = InstanceApplication.objects.all().order_by('filed')
+        if not (request.user.is_superuser or request.user.has_perm('ganeti.view_instances')):
+            applications = applications.filter(applicant = request.user)
+        app_list = []
+        i = 0
+        for app in applications:
+           appd = {}
+           appd['instance'] = app.hostname
+           i = i + 1
+           appd['count'] = i
+           appd['user'] = app.applicant.username
+           appd['time'] = (1000*mktime(app.filed.timetuple()))
+           app_list.append(appd)
+        cache.set('%s:ajaxapplist' %username,app_list, 90)
+    return HttpResponse(json.dumps(app_list), mimetype='application/json')
+
 
 @login_required
 def get_user_groups(request):
@@ -833,3 +840,13 @@ def disksizes(value):
 def memsize(value):
     return filesizeformat(value * 1024**2)
 
+@login_required
+@check_instance_auth
+def graph(request, cluster_slug, instance, graph_type):
+    try:
+        url = "http://stats.vima.grnet.gr/%s/%s.png" %(instance, graph_type)
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError:
+        pass
+    return HttpResponse(response.read(), mimetype="image/png")
