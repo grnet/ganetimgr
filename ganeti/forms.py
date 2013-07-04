7e9509f92f49112e6209fb771116848b83207387
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-07-04 13:19:04 +0300

    Add instance rename form

--- ganeti/forms.py
+++ ganeti/forms.py
@@ -19,9 +19,43 @@ from django.conf import settings
 from django.core.mail import mail_managers
 from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_unicode
+from ganetimgr.ganeti.models import Instance
+
+import re
+
+_VALID_NAME_RE = re.compile("^[a-z0-9.-]{1,255}$")
 
 class tagsForm(forms.Form):
     tags = forms.CharField()
+    
+
+class InstanceRenameForm(forms.Form):
+    hostname = forms.CharField(help_text=ugettext_lazy("A fully qualified domain name,"
+                                         " e.g. host.domain.com"), label=ugettext_lazy("Hostname"))
+    def clean_hostname(self):
+        hostname = self.cleaned_data["hostname"].rstrip(".")
+
+        # Check copied from ganeti's code
+        if (not _VALID_NAME_RE.match(hostname) or
+            # double-dots, meaning empty label
+            ".." in hostname or
+            # empty initial label
+            hostname.startswith(".")):
+            raise forms.ValidationError(_("Invalid hostname %s") % hostname)
+
+        if not hostname.count("."):
+            # We require at least two DNS labels
+            raise forms.ValidationError(mark_safe(_("Hostname should be fully"
+                                                    " qualified, e.g. <em>host"
+                                                    ".domain.com</em>, not"
+                                                    " <em>host</em>")))
+        existing_instances = Instance.objects.all()
+        names = [i.name for i in existing_instances]
+        if hostname in names:
+            raise forms.ValidationError(_("Hostname already exists."))
+        return hostname
+
