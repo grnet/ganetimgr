1a89fffcf295908e1a2ac7dad5f137512f274844
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-02-11 17:05:34 +0200

    Increase security with session timeout & passchange logout

--- accounts/models.py
+++ accounts/models.py
@@ -17,16 +17,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.signals import user_logged_in
 
+from datetime import datetime
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-
     first_login = models.BooleanField(default=True)
+    force_logout_date = models.DateTimeField(null=True, blank=True)
 
+    def force_logout(self):
+        self.force_logout_date = datetime.now()
+        self.save()
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
+
+def update_session_last_login(sender, user, request, **kwargs):
+    if request:
+        request.session['LAST_LOGIN_DATE'] = datetime.now()
+user_logged_in.connect(update_session_last_login)
