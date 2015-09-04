e050c797baa996311255bc4b1768154200c01bb2
Stauros Kroustouris <staurosk@noc.grnet.gr>
2015-09-04 14:54:36 +0300

    monkey patch manage.py

--- manage.py
+++ manage.py
@@ -23,5 +23,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ganetimgr.settings")
 
     from django.core.management import execute_from_command_line
+    import gevent
+    from gevent import monkey
+    monkey.patch_all()
 
     execute_from_command_line(sys.argv)
