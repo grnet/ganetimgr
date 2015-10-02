46c7d6275c6cba66784132893fae0dc1a9c46901
Stauros Kroustouris <staurosk@noc.grnet.gr>
2015-10-02 16:26:38 +0300

    ganeti app tests

--- ganeti/tests.py
+++ ganeti/tests.py
@@ -285,3 +285,117 @@ class InstancesTestCase(LoginTestCase):
         self.login_superuser()
         res = self.client.get(reverse('isolate', kwargs={'instance': 'test'}))
         self.assertEqual(res.status_code, 404)
+
+
+class JobsTestCase(LoginTestCase):
+    # the tests we can do here are really limited because this part
+    # of the app is heavilly dependent in the ganeti rapi. We can
+    # just make sure that we get the proper error messages
+    def setUp(self):
+        self.client = Client()
+        self.user_username = 'ganetitest'
+        self.user_password = 'ganetitest'
+        self.superuser_username = 'ganetitestadmin'
+        self.superuser_password = 'ganetitestadmin'
+        self.user = User.objects.create_user(self.user_username, 'test@test.com', self.user_password)
+        self.superuser = User.objects.create_user(self.superuser_username, 'test@test.com', self.superuser_password)
+        self.superuser.is_staff = True
+        self.superuser.is_superuser = True
+        self.superuser.save()
+
+    def test_jobs(self):
+        # should get a redirect to the login page
+        res = self.client.get(reverse('jobs'))
+        self.assertEqual(res.status_code, 302)
+
+        # should get a redirect instances
+        self.login_user()
+        res = self.client.get(reverse('jobs'))
+        self.assertEqual(res.status_code, 403)
+
+        # should raise 404 for superuser
+        # because instance does not exist
+        self.login_superuser()
+        res = self.client.get(reverse('jobs'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_jobs_json(self):
+        # should get a redirect to the login page
+        res = self.client.get(reverse('jobs_json'))
+        self.assertEqual(res.status_code, 302)
+
+        # should get a redirect instances
+        self.login_user()
+        res = self.client.get(reverse('jobs_json'))
+        self.assertEqual(res.status_code, 403)
+
+        # should raise 404 for superuser
+        # because instance does not exist
+        self.login_superuser()
+        res = self.client.get(reverse('jobs_json'))
+        self.assertEqual(res.status_code, 200)
+
+
+class NodegroupsTestCase(LoginTestCase):
+    # the tests we can do here are really limited because this part
+    # of the app is heavilly dependent in the ganeti rapi. We can
+    # just make sure that we get the proper error messages
+    def setUp(self):
+        self.client = Client()
+        self.user_username = 'ganetitest'
+        self.user_password = 'ganetitest'
+        self.superuser_username = 'ganetitestadmin'
+        self.superuser_password = 'ganetitestadmin'
+        self.user = User.objects.create_user(self.user_username, 'test@test.com', self.user_password)
+        self.superuser = User.objects.create_user(self.superuser_username, 'test@test.com', self.superuser_password)
+        self.superuser.is_staff = True
+        self.superuser.is_superuser = True
+        self.superuser.save()
+        self.cluster = Cluster.objects.create(
+            hostname='test.example.com',
+            slug='test'
+        )
+
+    def test_fromnet(self):
+        # should get a redirect to the login page
+        res = self.client.get(reverse('ng_from_net'))
+        self.assertEqual(res.status_code, 302)
+
+        # should get a redirect to login
+        self.login_user()
+        res = self.client.get(reverse('ng_from_net'))
+        self.assertEqual(res.status_code, 302)
+
+        # requires network_id in get params
+        self.login_superuser()
+        res = self.client.get(reverse('ng_from_net'))
+        self.assertEqual(res.status_code, 400)
+
+        # should raise 404 for superuser
+        # because network does not exist
+        res = self.client.get(reverse('ng_from_net'), {'network_id': 1})
+        self.assertEqual(res.status_code, 404)
+
+    def test_cluster_ng_stack(self):
+        # should get a redirect to the login page
+        res = self.client.get(reverse('cluster_ng_stack'))
+        self.assertEqual(res.status_code, 302)
+
+        # should get a redirect to login
+        self.login_user()
+        res = self.client.get(reverse('cluster_ng_stack'))
+        self.assertEqual(res.status_code, 302)
+
+        # should raise bad request
+        self.login_superuser()
+        res = self.client.get(reverse('cluster_ng_stack'))
+        self.assertEqual(res.status_code, 400)
+
+        # should raise 404 for superuser
+        # because cluster does not exist
+        res = self.client.get(reverse('cluster_ng_stack'), {'cluster_id': '120'})
+        self.assertEqual(res.status_code, 404)
+
+        # should return 200 (with error message)
+        res = self.client.get(reverse('cluster_ng_stack'), {'cluster_id': self.cluster.pk})
+        self.assertEqual(res.status_code, 200)
