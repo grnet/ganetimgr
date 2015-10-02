a86ef86c2e726c9a2f3d4f99d5335a5b970ada13
Stauros Kroustouris <staurosk@noc.grnet.gr>
2015-10-02 10:22:50 +0300

    fix threading warning

--- manage.py
+++ manage.py
@@ -22,9 +22,8 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ganetimgr.settings")
 
-    from django.core.management import execute_from_command_line
-    import gevent
     from gevent import monkey
     monkey.patch_all()
+    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
