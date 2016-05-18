dc6b630486ce9f7e5d7286986484c878064afd2a
Sergios Aftsidis <safts@noc.grnet.gr>
2016-05-18 12:05:18 +0300

    [ganetimgr1.7] Fix problematic migrations
    The migrations were messed up, possibly because since the Django 1.7 branch
    was introduced more migrations have been added.
    
    The problem was quite cryptic, kept getting the following error message:
    django.core.exceptions.ImproperlyConfigured:
    	Application labels aren't unique, duplicates: apply
    
    Searches kept pointing to ways to actually rename your app if it has the same
    label with another one, as Django 1.7 no longer allows this. This was not useful
    though, as no duplicate app existed. Not really sure what the problem actually was,
    but deleting existing migrations in the app fixed it.
    
    Afterwards, though, we could not create migrations for certain apps (eg. notifications).
    This happened as notifications had already some south migrations, probably trash left while
    merging the commits from 1.7 branch.
    
    To fix this:
    
    1) All (django) migrations were deleted
    2) All south-migrations for every app were added to <app>/south_migrations directory.
    3) ./manage.py makemigrations to create new initial django migrations
    4) ./manage.py migrate to (fake) apply the new migrations.

---
+++ notifications/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='NotificationArchive',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('subject', models.CharField(max_length=255)),
+                ('message', models.TextField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('recipients', models.ManyToManyField(related_name='notifications', to=settings.AUTH_USER_MODEL)),
+                ('sender', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'ordering': ['-date'],
+                'permissions': (('can_view_notif', 'Can view all notifications'),),
+            },
+            bases=(models.Model,),
+        ),
+    ]
